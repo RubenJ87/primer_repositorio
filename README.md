0f0669521a266a08f4386f4308787568f20af899
RubenJ87 <pocketi2@hotmail.com>
2013-05-02 11:05:24 +0100

    Mi primer commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Mostrando como usar un sistema de versiones
---
+++ hola_mundo.c
@@ -0,0 +1,10 @@
+//Rubén Jorge
+//Ejemplo Hola_Mundo java
+
+public class Hola_mundo{
+	public static void main(String[] args){
+	
+	System.out.println("Hola Mundo");	
+	
+	}
+}
